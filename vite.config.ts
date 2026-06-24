c044b7bd84d769ed9124f132534d111df82c128d
AdityaKhatri <adityakhatri47@gmail.com>
2026-06-24 17:29:22 +0545

    fix: bottom nav extra space in ios

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    host: true,
+  },
   build: {
     target: 'es2020',
   },
